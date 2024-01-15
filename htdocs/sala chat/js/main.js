e05401cae8a0ab749fcb789d2a8d11eab1e639c0
AF0ns3ca <afonsecahdez@gmail.com>
2024-01-15 09:11:01 +0100

    primeras funciones

--- htdocs/sala chat/js/main.js
+++ htdocs/sala chat/js/main.js
@@ -2,3 +2,16 @@
  * @author Alvaro Fonseca Hernandez
  * @github
  */
+
+//Funciones de utilidad
+const $ = id => {return document.getElementById(id)};
+const log = msg => {
+    $("log").innerHTML += "<br>" + msg;
+    //Cada vez que escribimos hacemos scroll vertical cuando sea necesario
+    $("log").scrollTop = $("log").scrollHeight;
+};
+
+const onkey = event => {if (event.keyCode == 13) {send();}};
+
+//Variable global para instanciar el WebSocket
+let socket
